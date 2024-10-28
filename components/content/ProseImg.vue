d3cd9a8c0fc6762762d3e26bc0c6ec6ffbdfb238
bolokoz <yurifbeck@gmail.com>
2024-10-28 10:46:02 -0400

    trying to fix image

---
+++ components/content/ProseImg.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+const props = defineProps({
+  src: {
+    type: String,
+    default: '',
+  },
+  alt: {
+    type: String,
+    default: '',
+  },
+  width: {
+    type: [String, Number],
+    default: undefined,
+  },
+  height: {
+    type: [String, Number],
+    default: undefined,
+  },
+})
+</script>
+
+<template>
+  <NuxtImg
+    loading="lazy"
+    :src="src"
+    :alt="props.alt"
+    sizes="100vw sm:50vw md:400px"
+    placeholder
+    :modifiers="{ roundCorner: '20:40' }"
+    :width="props.width"
+    :height="props.height"
+  />
+</template>
+
+<style>
+
+</style>
